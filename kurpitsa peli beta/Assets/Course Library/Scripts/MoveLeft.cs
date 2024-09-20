605c93a0d082bd74039c2b2ad1d879d8d12948e0
LeoJuusto <s2200814@edu.bc.fi>
2024-09-20 09:30:09 +0300

    yupdate

--- kurpitsa peli beta/Assets/Course Library/Scripts/MoveLeft.cs
+++ kurpitsa peli beta/Assets/Course Library/Scripts/MoveLeft.cs
@@ -6,7 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 10;
     private float leftBound = -15;
-    // Start is called before the first frame update
+
     void Start()
     {
     
